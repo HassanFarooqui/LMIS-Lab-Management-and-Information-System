1089f752a1a775f83722d784f6d69b71d8e8e200
Hassan Farooqui <hassan_farooqui90@yahoo.com>
2020-06-28 21:23:40 +0500

    Calculate discount amount and net charges on test charges input

--- src/app/addtest/addtest.component.ts
+++ src/app/addtest/addtest.component.ts
@@ -10,13 +10,24 @@ import { Router, ActivatedRoute, ParamMap } from "@angular/router";
   styleUrls: ['./addtest.component.css']
 })
 export class AddtestComponent implements OnInit {
-
+  TestName: any;
+  TestCharges: any;
+  TestDiscPerc : number;
+  TestDiscAmount : number;
+  TestNetcharges : number;
   constructor(private appService: AppServiceService,
     private snackbar: MatSnackBar,
     private routeParams: ActivatedRoute,
     private router : Router) { }
 
   ngOnInit(): void {
+  }
+
+  calculateDiscount(){
+    if(this.TestDiscPerc != undefined && this.TestDiscPerc > 0){
+      this.TestDiscAmount =Math.round((this.TestCharges/100)*this.TestDiscPerc);
+      this.TestNetcharges = Math.round(this.TestCharges - this.TestDiscAmount); 
+    }
   }
 
 }
