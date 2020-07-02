e5c98e484a3b602135a63f6287e75b18dfded5fe
Hassan Farooqui <hassan_farooqui90@yahoo.com>
2020-07-02 23:29:00 +0500

    Calculate Discount on table and improve design of Add Package Screen

--- src/app/addpackage/addpackage.component.ts
+++ src/app/addpackage/addpackage.component.ts
@@ -1,84 +1,95 @@
 import { Component, OnInit } from '@angular/core';
 import { IDropdownSettings } from 'ng-multiselect-dropdown';
 import { AppServiceService } from '../app-service.service';
-import {MatTableModule, MatTableDataSource} from '@angular/material/table';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { FormControl } from '@angular/forms';
+
+interface Package {
+  TestId: number;
+  TestName: string;
+  TestTypeId: number
+  TestCharges: number;
+  TestDiscPerc: number;
+  TestDiscAmount: number;
+  NetCharges: number;
+}
+
 @Component({
   selector: 'app-addpackage',
   templateUrl: './addpackage.component.html',
   styleUrls: ['./addpackage.component.css']
 })
 export class AddpackageComponent implements OnInit {
-  displayedColumns: string[] = ['TestID', 'TestName' ,'TestCharges', 'TestDiscPerc', 'TestDiscAmount' , 'NetCharges', 'Actions'];
+  displayedColumns: string[] = ['TestID', 'TestName', 'TestCharges', 'TestDiscPerc', 'TestDiscAmount', 'NetCharges', 'Actions'];
   //displayedColumns: string[] = ['TestID', 'TestName' ];
   originalTestList = [];
   selectedItems = [];
-  selectItemData : any;
-  PackageName : any;
-  Incentive : boolean = false;
-  Active : boolean = false;
-  IsPartial : boolean = false;
-  CreditLimit : any;
-  Totaldiscount : any;
+  selectItemData: any;
+  PackageName: any;
+  Incentive: boolean = false;
+  Active: boolean = false;
+  IsPartial: boolean = false;
+  CreditLimit: any;
+  Totaldiscount: any;
   dropdownSettings: IDropdownSettings;
   multipleTestList = new FormControl();
   multiSelectDataSource = [];
-  multiSelectId : any = [];
+  multiSelectId: any = [];
+  TestListDataSource: Array<Package> = [];
 
 
   constructor(private appService: AppServiceService) { }
-  TestListDataSource : any = []
+
   ngOnInit(): void {
-    this.getTestList(); 
-    // this.selectedItems = [
-    //   { item_id: 3, item_text: 'Pune' },
-    //   { item_id: 4, item_text: 'Navsari' }
-    // ];
- 
+    this.getTestList();
+
   }
   onItemSelect(item: any) {
     this.selectedItems.push(item.TestName);
-   
-
-    this.TestListDataSource = new MatTableDataSource(this.selectedItems);
+    //this.TestListDataSource = new MatTableDataSource(this.selectedItems);
   }
   onSelectAll(items: any) {
-    console.log(items);
+   this.selectedItems.push(items.TestName);
   }
 
-  getTestList(){    
+  getTestList() {
     this.appService.listOfTest().subscribe(
       response => {
-        if(response.success){
-        if(response.model.length > 0){
-          this.originalTestList = response.model;
-                response.model.forEach(element => {
-                  var obj = {
-                    TestId : element.TestId,
-                    TestName: element.TestId + ' | ' + element.TestName + ' | Rs: ' + element.TestCharges + ' | Disc: ' + element.TestDiscPerc                 
-                   } 
-                   this.multiSelectDataSource.push(obj);
-                 });
+        if (response.success) {
+          if (response.model.length > 0) {
+            this.originalTestList = response.model;
+            response.model.forEach(element => {
+              var obj = {
+                TestId: element.TestId,
+                TestName: element.TestId + ' | ' + element.TestName + ' | Rs: ' + element.TestCharges + ' | Disc: ' + element.TestDiscPerc
               }
+              this.multiSelectDataSource.push(obj);
+            });
+          }
         }
       }
     );
   }
 
-  addTestInList(){
+  addTestInList() {
     this.TestListDataSource = [];
     this.multiSelectId.forEach(element => {
-     var obj = this.originalTestList.filter(x => x.TestId == element)[0];
-     this.TestListDataSource.push(obj);     
+      var obj = this.originalTestList.filter(x => x.TestId == element)[0];
+      this.TestListDataSource.push(obj);
     });
   }
 
-  calculateDiscount(){
-    
-    
-    if (this.TestListDataSource.TestDiscPerc != undefined && this.TestListDataSource.TestDiscPerc > 0){
-      this.TestListDataSource.TestDiscAmount = Math.round((this.TestListDataSource.TestCharges / 100) * this.TestListDataSource.TestDiscPerc);
-      this.TestListDataSource.TestNetcharges = Math.round(this.TestListDataSource.TestCharges - this.TestListDataSource.TestDiscAmount);
-    } 
+  calculateDiscount(discountPerc: any, element) {
+    if(discountPerc > 100){
+       discountPerc = 0;
+    }
+    else if (discountPerc != undefined && discountPerc > 0) {
+      element.TestDiscAmount = Math.round((element.TestCharges / 100) * discountPerc);
+      element.NetCharges = Math.round(element.TestCharges - element.TestDiscAmount);
+    }
+  }
+
+  DeleteTestInGrid(){
+    console.log('2');
   }
 }
